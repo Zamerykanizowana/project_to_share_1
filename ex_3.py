236ff760acfbe7cf49a568b36d974f4291562306
Kamil Wrzos <kamilwrzos999@gmail.com>
2023-04-24 12:33:25 +0200

    Dodanie zadania 3

--- ex_3.py
+++ ex_3.py
@@ -1,5 +1,13 @@
 # Policz studentki i studentow i zwroc wynik w formacie: [k, m]
 # Przyjmij, ze imie dla kobiety konczy sie na "a"
 def policz_studentow_plec(studenci) -> [int, int]:
-    # TODO
-    return [0, 0]
+    k = 0  # liczba studentek
+    m = 0  # liczba studentów
+    
+    for student in studenci:
+        if student[-1] == 'a':
+            k += 1
+        else:
+            m += 1
+            
+    return [k, m]
